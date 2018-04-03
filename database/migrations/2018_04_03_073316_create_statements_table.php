5665c68622aaa27f5a1c913746232a0e38077eb1
espilioto <Manolis@ManolisDev.argosystems.gr>
2018-04-03 10:35:00 +0300

    Created migrations

---
+++ database/migrations/2018_04_03_073316_create_statements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->integer('account_id')->unsigned();
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->decimal('amount', 8, 2);
+            $table->string('notes');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->boolean('isLoan')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statements');
+    }
+}

61bd5666d13678b0ae80e92cacf16bb0e84b536c
espilioto <Manolis@ManolisDev.argosystems.gr>
2018-04-03 10:42:15 +0300

    Added StatementController

---
+++ app/Http/Controllers/StatementController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Statement;
+use Illuminate\Http\Request;
+
+class StatementController extends Controller
+{
+    public function showAllStatements()
+    {
+        return response()->json(Statement::with('account', 'category')->get());
+    }
+
+    public function showOneStatement($id)
+    {
+        return response()->json(Statement::where('id', $id)->with('account', 'category')->get());
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'date' => 'required|date',
+            'account_id' => 'required',
+            'amount' => 'required',
+            'notes' => 'required',
+            'category_id' => 'required',
+            'isLoan' => 'required',
+        ]);
+
+        $statement = Statement::create($request->all());
+
+        return response()->json($statement, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $statement = Statement::findOrFail($id);
+        $statement->update($request->all());
+
+        return response()->json($statement, 200);
+    }
+
+    public function delete($id)
+    {
+        Statement::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+}

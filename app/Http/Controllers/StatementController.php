36d28726f3385ec4eb38341c35080e4e6213251b
espilioto <espilioto@users.noreply.github.com>
2018-04-05 10:41:35 +0300

    Api key authentication

--- app/Http/Controllers/StatementController.php
+++ app/Http/Controllers/StatementController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class StatementController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function showAllStatements()
     {
         return response()->json(Statement::with('account', 'category', 'user')->get());
